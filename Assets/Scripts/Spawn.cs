a36cd89d9a81fff2d75a4c6ac1c84c0e8af7f3c7
452886 <452886@student.saxion.nl>
2020-02-01 15:51:38 +0100

    Added a basic item spawner

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    GameObject currentItem;
+
+    public void FillSpawn(GameObject item)
+    {
+        currentItem = item;
+    }
+
+    public GameObject CurrentItem
+    {
+        get { return currentItem; }
+    }
+    public void ClearItem()
+    {
+        this.currentItem = null;
+    }
+}
