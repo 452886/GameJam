b0a8bb11f2481ceab1c2bdf3c36e4925c8ac6283
Dennis Roelvink <dennis.roelvink@topicus.nl>
2020-02-01 02:21:28 +0100

    movement

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PhysicsObject : MonoBehaviour
+{
+    [Header("Physics Object")]
+    [SerializeField] float gravity = -20f;
+    
+    private CharacterController controller;
+    private float currentGravity = 0f;
+    private Vector3 velocity;
+
+    // Start is called before the first frame update
+    public virtual void Start()
+    {
+        this.controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    public virtual void Update()
+    {
+        UpdateGravity();
+        MoveCharacter();
+    }
+
+    public void SetVelocity(Vector3 velocity) {
+        this.velocity = velocity;
+    }
+
+    public void UpdateGravity() {
+        currentGravity += gravity * Time.deltaTime;
+        
+        if(controller.isGrounded) currentGravity = -3f;
+        velocity += Vector3.up * currentGravity;
+    }
+
+    public void MoveCharacter() {
+        this.controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    [Header("Player Controller")]
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float acceleration = 2f;
+    [SerializeField] float deceleration = 5f;
+    private Vector3 moveVelocity;
+
+    private float xAxis, yAxis;
+
+    // Start is called before the first frame update
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    public override void Update()
+    {
+
+        var xAxis = Input.GetAxisRaw("Horizontal");
+        var yAxis = Input.GetAxisRaw("Vertical");
+
+        SetVelocity(new Vector3(xAxis, 0f, yAxis).normalized * movementSpeed);
+        
+        UpdateGravity();
+        MoveCharacter();
+    }
+}
