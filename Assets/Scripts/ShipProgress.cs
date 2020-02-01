19bf3b9dd7fac286c5515f2f515395d6ed85d802
Dennis Roelvink <dennis.roelvink@topicus.nl>
2020-02-01 23:27:58 +0100

    UI for ship repair status

--- Assets/Scripts/ShipProgress.cs
+++ Assets/Scripts/ShipProgress.cs
@@ -1,20 +1,34 @@
 ﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ShipProgress : MonoBehaviour
 {
     [SerializeField] Team team;
     [SerializeField] Constants constants;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Slider slider;
+    
     public delegate void ShipCompleted(Team team);
-
     public event ShipCompleted shipCompleted;
 
+    private int currentMaximusRepairus;
 
-    public int currentMaximusRepairus;
+    void Start()
+    {
+        slider.maxValue = constants.shipRepairWinCondition;
+    }
 
     public void AddRepairPoints(int points) {
         this.currentMaximusRepairus += points;
         if(this.currentMaximusRepairus <= 0) this.currentMaximusRepairus = 0;
         if(this.currentMaximusRepairus >= constants.shipRepairWinCondition && shipCompleted != null) shipCompleted(team); 
+    }
+
+    void FixedUpdate()
+    {
+        slider.value = this.currentMaximusRepairus;
+        text.text = string.Format("{0} / {1}", currentMaximusRepairus, constants.shipRepairWinCondition);
     }
 
 }
