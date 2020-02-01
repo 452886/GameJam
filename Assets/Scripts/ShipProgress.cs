66a54b0d8b08ff37cf97156aa0766c32e9d644a2
Dennis Roelvink <dennis.roelvink@topicus.nl>
2020-02-01 22:51:53 +0100

    ship points

---
+++ Assets/Scripts/ShipProgress.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ShipProgress : MonoBehaviour
+{
+    [SerializeField] Team team;
+    [SerializeField] Constants constants;
+    public delegate void ShipCompleted(Team team);
+
+    public event ShipCompleted shipCompleted;
+
+
+    public int currentMaximusRepairus;
+
+    public void AddRepairPoints(int points) {
+        this.currentMaximusRepairus += points;
+        if(this.currentMaximusRepairus <= 0) this.currentMaximusRepairus = 0;
+        if(this.currentMaximusRepairus >= constants.shipRepairWinCondition && shipCompleted != null) shipCompleted(team); 
+    }
+
+}
+
+[System.Serializable]
+public enum Team {
+    BLUE,
+    RED
+}
---
+++ Assets/Scripts/WinconditionChecker.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class WinconditionChecker : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectsWithTag("Ship").ToList().ForEach(ship => {
+            ship.GetComponent<ShipProgress>().shipCompleted  += OnShipCompletion;
+        });
+    }
+
+    public void OnShipCompletion(Team team) {
+        Debug.Log(string.Format("LOL TEAM {0} WON!", team == Team.BLUE ? "BLUE" : "RED"));
+    }
+}
